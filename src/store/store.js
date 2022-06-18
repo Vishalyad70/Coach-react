8ac0b8cfa8e1305536031851689824885ba03bb6
“Vishal <ahan3445@gmail.com>
2022-06-18 19:55:47 +0530

    changes

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,11 @@ import rootReducer from "./reducers/rootReducer";
 import thunkMiddleware from "redux-thunk";
 
 const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunkMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+    rootReducer,
+    compose(
+        applyMiddleware(thunkMiddleware),
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 
 export default store;
